871851b0c858481ad402ec2b484526dbcbd004a3
faycal-jebali <39644707+faycal-jebali@users.noreply.github.com>
2023-06-22 22:48:44 +0200

    Rxjs/hello world

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import './style.css';
 import { of, map } from 'rxjs';
 
 of('World')
-  .pipe(map((name) => `Hello, ${name}!`))
+  .pipe(map((name) => `Hello  ${name}!`))
   .subscribe(console.log);
 
 // Open the console in the bottom right to see results.
